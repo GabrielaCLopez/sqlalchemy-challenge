b3ba420353eb7df2416e4bd9fe240dcd8091c00d
GabrielaCLopez <78400652+GabrielaCLopez@users.noreply.github.com>
2023-02-27 16:55:42 -0800

    gathered information for most active station

--- SurfsUp/.ipynb_checkpoints/app-checkpoint.py
+++ SurfsUp/.ipynb_checkpoints/app-checkpoint.py
@@ -79,6 +79,24 @@ def stations():
     
     return jsonify(stations_list)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine) 
+    
+    # Get the dates and temperature observations of the most active station
+    station_data = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date>='2016-08-23') \
+                                                              .filter(Measurement.date<='2017-08-23') \
+                                                              .filter(Measurement.station=='USC00519281') \
+                                                              .all()
+    
+    
+    # Createa a dictionary of the dates and temperatures for the station
+    station_info={date:tobs for date, tobs in station_data}
+
+    # return a list of the temperature data for the most active station
+    return jsonify(station_info)
+
 
 
 if __name__ == '__main__':
